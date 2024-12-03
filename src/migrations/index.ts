d606be4edafba95bb2b933d90f7525b0c4dd9a90
Okan <okan.unallar@gmail.com>
2024-12-03 20:47:49 +0300

    migration options updated

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241203_171816 from './20241203_171816';
+import * as migration_20241203_174534 from './20241203_174534';
 
 export const migrations = [
   {
     up: migration_20241203_171816.up,
     down: migration_20241203_171816.down,
-    name: '20241203_171816'
+    name: '20241203_171816',
+  },
+  {
+    up: migration_20241203_174534.up,
+    down: migration_20241203_174534.down,
+    name: '20241203_174534'
   },
 ];
