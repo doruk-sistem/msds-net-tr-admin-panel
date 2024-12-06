aa016ddf372191c43544f44548066b2be0fdc251
Okan <okan.unallar@gmail.com>
2024-12-06 22:18:40 +0300

    migration files created

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241203_171816 from './20241203_171816';
 import * as migration_20241203_174534 from './20241203_174534';
+import * as migration_20241206_191808_new from './20241206_191808_new';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20241203_174534.up,
     down: migration_20241203_174534.down,
-    name: '20241203_174534'
+    name: '20241203_174534',
+  },
+  {
+    up: migration_20241206_191808_new.up,
+    down: migration_20241206_191808_new.down,
+    name: '20241206_191808_new'
   },
 ];
