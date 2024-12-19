1306f73b97a5c0959c334aac367e624075b2999e
Okan <okan.unallar@gmail.com>
2024-12-19 13:37:43 +0300

    secret test

--- src/app/(payload)/api/auth/[...nextauth]/authOptions.ts
+++ src/app/(payload)/api/auth/[...nextauth]/authOptions.ts
@@ -157,7 +157,8 @@ const authOptions: AuthOptions = {
       return session
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  // secret: process.env.NEXTAUTH_SECRET,
+  secret: '7e221b8caf590d540e2c43bb465511b129af897e5b338bbdb4bdd8582acf317b',
 }
 
 function refreshAccessToken(token: JWT): JWT {
