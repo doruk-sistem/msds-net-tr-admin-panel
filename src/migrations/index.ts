5379940aab0d56b4377957397b237bde7e2c7b6a
Okan <okan.unallar@gmail.com>
2025-01-06 15:15:11 +0300

    migration files restored

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
-import * as migration_20250106_113244_initial from './20250106_113244_initial';
+import * as migration_20241219_141241_inital from './20241219_141241_inital';
+import * as migration_20250106_110124_v0_0_1_beta_9 from './20250106_110124_v0_0_1_beta_9';
 
 export const migrations = [
   {
-    up: migration_20250106_113244_initial.up,
-    down: migration_20250106_113244_initial.down,
-    name: '20250106_113244_initial'
+    up: migration_20241219_141241_inital.up,
+    down: migration_20241219_141241_inital.down,
+    name: '20241219_141241_inital',
+  },
+  {
+    up: migration_20250106_110124_v0_0_1_beta_9.up,
+    down: migration_20250106_110124_v0_0_1_beta_9.down,
+    name: '20250106_110124_v0_0_1_beta_9'
   },
 ];
