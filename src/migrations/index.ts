2f332426aa8741f88aa68fa95c5df967e1acd623
Okan <okan.unallar@gmail.com>
2025-01-06 14:33:17 +0300

    migration files updated

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20241219_141241_inital from './20241219_141241_inital';
-import * as migration_20250106_110124_v0_0_1_beta_9 from './20250106_110124_v0_0_1_beta_9';
+import * as migration_20250106_113244_initial from './20250106_113244_initial';
 
 export const migrations = [
   {
-    up: migration_20241219_141241_inital.up,
-    down: migration_20241219_141241_inital.down,
-    name: '20241219_141241_inital',
-  },
-  {
-    up: migration_20250106_110124_v0_0_1_beta_9.up,
-    down: migration_20250106_110124_v0_0_1_beta_9.down,
-    name: '20250106_110124_v0_0_1_beta_9'
+    up: migration_20250106_113244_initial.up,
+    down: migration_20250106_113244_initial.down,
+    name: '20250106_113244_initial'
   },
 ];
