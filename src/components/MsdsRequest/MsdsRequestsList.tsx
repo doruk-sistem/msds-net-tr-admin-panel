4e26ced40468211d10969b45cc5da07a39dc32fd
alicankocman <kocmanalican@gmail.com>
2025-03-21 10:09:41 +0300

    Remove debug console.log

--- src/components/MsdsRequest/MsdsRequestsList.tsx
+++ src/components/MsdsRequest/MsdsRequestsList.tsx
@@ -43,7 +43,6 @@ export const MsdsRequestsList = () => {
 
   const fetchRequests = async () => {
     if (!user) return
-    console.log('Current user:', user)
     try {
       setIsLoading(true)
     
